e6f9be8864bd9ffef86c91a4095e07c9d565d251
Harkirat Singh <innosam@gmail.com>
2015-06-23 20:08:50 +0000

    add queue support for multiple request

--- rasp.py
+++ rasp.py
@@ -1,26 +1,33 @@
+from collections import deque
 from flask import Flask
 from flask import request
-import subprocess
+from multiprocessing import Process, Pipe
+from subprocess import Popen, PIPE
+
+a, b = Pipe()
 
 app = Flask(__name__)
-app.debug = True
 
-class VideoManageri(object):
-  queue = []
-  def enqueue_song(video_url):
-     pass
-  
-  def 
-
+def videomanager():
+    vidMgrQueue = deque([])
+    while True:
+	vidMgrQueue.append(b.recv())
+        if vidMgrQueue:
+            video_url = vidMgrQueue.popleft()
+            print "Playing %s"%video_url
+	    cmd='omxplayer -o local --vol -2000 "$(youtube-dl -g \'%s\')"'%video_url
+            sub = Popen("nohup "+ cmd, shell=True)
+            out, err = sub.communicate()
 
 
 @app.route('/')
 @app.route('/omxplayer', methods=['GET','POST'])
 def omxplayer():
     video_url = request.args.get('video_url', '')
-    cmd='omxplayer -o local --vol -2000 "$(youtube-dl -g \'%s\')"'%video_url
-    subprocess.Popen("nohup "+ cmd, shell=True)
-    return "video will start playing in a few seconds"
+    a.send(video_url)
+    return "video is added to the queue"
 
 if __name__ == '__main__':
+    proc = Process(target=videomanager)
+    proc.start()
     app.run(host='0.0.0.0')
