8e25adbfa811ca0b02e50333151135563d6860a8
Harkirat Singh <innosam@gmail.com>
2015-12-20 17:17:07 +0000

    refactor

---
+++ app/player.py
@@ -0,0 +1,62 @@
+import gevent
+from collections import deque
+from multiprocessing import Process, Pipe
+from gevent.subprocess import Popen, PIPE
+from gevent import monkey
+from gevent.socket import wait_read, wait_write
+from omxdbus import OmxDbus
+import json, cPickle
+
+
+class Player:
+     
+    def __init__(self, pipe_right, pipe_status_right):
+        self.pipe_right = pipe_right
+        self.pipe_status_right = pipe_status_right
+        gevent.spawn(self.status_thread)
+        self.sub = None
+
+    def manager(self):
+        print "Starting Manager"
+        while True: 
+            wait_read(self.pipe_right.fileno())
+            data = self.pipe_right.recv()
+            self.process_data(data)  
+
+    def process_Data(self, data):
+        data = cPickle.loads(self.data)
+  	if(data.has_key('cmd')):
+           self.process_cmd(data['cmd'])
+
+        if(data.has_key('video_url')):
+           self.process_video_url(data['video_url'])    
+
+    def process_video_url(self, video_url):
+        print "Playing %s"%video_url
+        cmd=['youtube-dl', '-g','-f best',video_url]
+        temp = Popen(cmd, stdout=PIPE)
+        out, err = temp.communicate()
+        video_direct = out
+        cmd=['omxplayer', '-o', 'hdmi',  '--vol', '-2000', video_direct[:-1]]
+        print " ".join(cmd)
+        self.sub = Popen(cmd, stdin=PIPE)
+        omxdbus = OmxDbus()
+        self.sub.wait()
+        print "done running the process"
+        self.sub = None
+     
+    def process_cmd(self, cmd):
+        OmxDbus().send_cmd(cmd)
+    
+    def status_thread(self):
+        statusQueue = deque([])
+        while True:
+            wait_read(self.pipe_status_right.fileno())
+            statusQueue.append(self.pipe_status_right.recv())
+            if statusQueue:
+                status = statusQueue.popleft()
+                result = ""
+                if(OmxDbus.is_initialized()):
+                    result = json.dumps(OmxDbus().get_status())
+                self.pipe_status_right.send(result)
+ 
--- rasp.py
+++ rasp.py
@@ -8,18 +8,14 @@ from gevent import monkey
 from gevent.socket import wait_read, wait_write
 from flask import render_template, send_from_directory
 from app.omxdbus import OmxDbus
-import json
+import json, cPickle
+from app.player import Player
 
 monkey.patch_socket()
 
-a, b = Pipe()
-c, d = Pipe()
-e, f = Pipe()
+pipe_left, pipe_right = Pipe()
+pipe_status_left, pipe_status_right = Pipe()
 
-
-dbusIfaceProp = None
-dbusIfaceKey = None
-done = 0
 
 app = Flask(__name__)
 sub = None
@@ -40,13 +36,16 @@ def send_fonts(path):
 @app.route('/omxplayer', methods=['GET','POST'])
 def omxplayer():
     cmd = request.args.get('cmd','')
+    dict_cmd = {'cmd': cmd}
     if cmd:
-        c.send(cmd)
+        pipe_left.send(cPickle.dumps(dict_cmd))
         return ""
 
     video_url = request.args.get('video_url', '')
-    a.send(video_url)
+    dict_video_url = {'video_url': video_url}
+    pipe_left.send(cPickel.dumps(dict_video_url))
     return ""
+
 
 @app.route('/omxplayer/add_video', methods=['GET'])
 def add_video():
@@ -73,9 +72,9 @@ def get_videos():
 
 @app.route('/omxplayer/status', methods=['GET'])
 def omxplayer_status():
-    e.send("get_status")
-    wait_read(e.fileno())
-    status = e.recv()
+    pipe_status_left.send("get_status")
+    wait_read(pipe_status_left.fileno())
+    status = pipe_status_left.recv()
     return status
 
 
@@ -83,56 +82,9 @@ def omxplayer_status():
 def raspberry():
     return render_template('rasp.html');
 
-
-
-def videomanager():
-    gevent.spawn(cmd_thread)
-    gevent.spawn(status_thread)
-    vidMgrQueue = deque([])
-    while True:
-        wait_read(b.fileno())
-        vidMgrQueue.append(b.recv())
-        if vidMgrQueue:
-            video_url = vidMgrQueue.popleft()
-            print "Playing %s"%video_url
-            cmd=['youtube-dl', '-g','-f best',video_url]
-            temp = Popen(cmd, stdout=PIPE)
-            out, err = temp.communicate()
-            video_direct = out
-            cmd=['omxplayer', '-o', 'hdmi',  '--vol', '-2000', video_direct[:-1]]
-            print " ".join(cmd)
-            sub = Popen(cmd, stdin=PIPE)
-	    omxdbus = OmxDbus()
-            sub.wait()
-            print "done running the process"
-
-
-def cmd_thread():
-    cmdQueue = deque([])
-    while True:
-        wait_read(d.fileno())
-        cmdQueue.append(d.recv())
-        if cmdQueue:
-            cmd = cmdQueue.popleft()
-            OmxDbus().send_cmd(cmd)
-
-def status_thread():
-    statusQueue = deque([])
-    while True:
-        wait_read(f.fileno())
-        statusQueue.append(f.recv())
-        if statusQueue:
-            status = statusQueue.popleft()
-            result = ""
-            if(OmxDbus.is_initialized()):
-                result = json.dumps(OmxDbus().get_status())
-            f.send(result)
-           
- 
  
 if __name__ == '__main__':
-    proc = Process(target=videomanager)
+    proc = Process(target = Player(pipe_right, pipe_status_right).manager)
     proc.start()
     app.run(host='0.0.0.0', port=80)
-
 
